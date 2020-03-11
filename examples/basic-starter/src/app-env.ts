5c0115ef578c23a2b488970d0b1c464f4251a28c
Hiroki Gota <hiroki.gota@spatialvision.com.au>
2020-03-11 14:12:26 +1100

    updated node runtime

--- examples/basic-starter/src/app-env.ts
+++ examples/basic-starter/src/app-env.ts
@@ -14,52 +14,8 @@ const initApiVersion = () => {
 };
 
 class AppEnv {
-  flattern (instance: any, envKey: string): any  {
-    const flat: any = {};
-    const leaves = traverse(instance).reduce(function (acc: any[], x: any) {
-      const self = this;
-
-      if(self.isLeaf) {
-        if(x === undefined) {
-          console.error('self.key: %s has no value specified: %s', self.key, x);
-          throw new Error(`Specify the value of ${self.key} for ${envKey}`);
-        }
-
-        flat[self.key as string] = x;
-      }
-
-      return acc;
-    }, []);
-
-    return flat;
-  }
-  init (targetEnv: any) {
+  init () {
     initApiVersion();
-
-    if(!targetEnv) {
-      log.info('initEnv targetEnv is empty using .env');
-      return;
-    }
-
-    const envs = require('../envs.json');
-
-    log.info('initEnv available env keys: %j', Object.keys(envs), envs[targetEnv]);
-
-    const envToFlattern = envs[targetEnv];
-
-    if(!envToFlattern) {
-      log.info('initEnv envs[targetEnv] is empty using .env');
-      return;
-    }
-
-    const flats = this.flattern(envToFlattern, targetEnv);
-
-    Object.keys(flats).forEach(key => {
-      // log.info('initEnv key: %s, env[key]: %j', key, env[key]);
-      process.env[key] = flats[key];
-      log.info('initEnv key: %s, value: %s', key, process.env[key]);
-    });
-
   }
 }
 const env = new AppEnv();
